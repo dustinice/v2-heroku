894f8ab9bbbb7554cdce1a65ca88726486a6cd48
yeahwu <yeahwu@users.noreply.github.com>
2020-08-26 17:56:58 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -4,37 +4,35 @@ wget -c https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux
 unzip v2ray-linux-64.zip
 chmod 700 v2ray v2ctl
 
-# Remove temporary directory
+# Remove v2ray.zip
 rm -f v2ray-linux-64.zip
 
-# V2ray new v2ray.sh
+# V2ray new v2ray.json
 cat << EOF > /config.json
-{	
-    "inbounds": 	
-    [	
-        {	
-            "port": $PORT,"protocol": "vmess",	
-            "settings": {"clients": [{"id": "$UUID"}],"decryption": "none"},	
-            "streamSettings": {"network": "ws"}	
-        }	
-    ],	
-    "outbounds": 	
-    [	
-        {"protocol": "freedom"},	
-        {"protocol": "socks","tag": "socksTor","settings": {"servers": [{"address": "127.0.0.1","port": 9050}]}}	
-    ],	
-    	
-    "routing": 	
-    {	
-        "rules": 	
-        [	
-            {"type": "field","outboundTag": "socksTor","domain": ["geosite:tor"]},	
-            {"type": "field","outboundTag": "blocked","domain": ["geosite:category-ads-all"]}	
-        ]	
-    }	
+{
+    "inbounds": [
+        {
+            "port": $PORT,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID"
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "ws"
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
 }
 EOF
 
 # Run v2ray
-nohup tor &
 ./v2ray -config /config.json
